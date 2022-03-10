97c98f0e6ca43f011e1774c91593571feba3bc5b
Matsushima Goro <139710425+poker90102@users.noreply.github.com>
2022-03-10 22:20:47 +0300

    Drop Example pods dependencies.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,17 +15,17 @@ let excludeFiles: String = []
 let package = Package(
     name: "Web3swift",
     platforms: [
-        .macOS(.v10_12), .iOS(.v11)
+        .macOS(.v10_12), .iOS(.v11),
     ],
     products: [
-        .library(name: "web3swift", targets: ["web3swift"])
+        .library(name: "web3swift", targets: ["web3swift"]),
     ],
     
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.2"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.4"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
     ],
     targets: [
         .target(name: "secp256k1"),
@@ -40,26 +40,7 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "localTests",
-            dependencies: ["web3swift"],
-            path: "Tests/web3swiftTests/local_tests",
-            resources: [
-                .copy("../../../TestToken/Helpers/SafeMath/SafeMath.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/ERC20.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/IERC20.sol"),
-                .copy("../../../TestToken/Token/Web3SwiftToken.sol")
-            ]
-        ),
-        .testTarget(
-            name: "remoteTests",
-            dependencies: ["web3swift"],
-            path: "Tests/web3swiftTests/infura_tests",
-            resources: [
-                .copy("../../../TestToken/Helpers/SafeMath/SafeMath.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/ERC20.sol"),
-                .copy("../../../TestToken/Helpers/TokenBasics/IERC20.sol"),
-                .copy("../../../TestToken/Token/Web3SwiftToken.sol")
-            ]
-        ),
+            name: "web3swiftTests",
+            dependencies: ["web3swift"]),
     ]
 )
